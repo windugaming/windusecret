ac70a0e1f94540ee2bbd5cdd13783715c69961e1
suki ajaib <tristanrakhaprasetyo@gmail.com>
2025-04-07 01:07:25 -0700

    Add files via upload

---
+++ video viral 8menit.bat
@@ -0,0 +1,53 @@
+@echo off
+
+CHCP 65001 >nul
+mode con: cols=900 lines=500
+:menu 
+echo ██╗     ██╗   ██╗    ██╗  ██╗███████╗███╗   ██╗ █████╗     ██╗  ██╗ █████╗  ██████╗██╗  ██╗
+echo ██║     ██║   ██║    ██║ ██╔╝██╔════╝████╗  ██║██╔══██╗    ██║  ██║██╔══██╗██╔════╝██║ ██╔╝
+echo ██║     ██║   ██║    █████╔╝ █████╗  ██╔██╗ ██║███████║    ███████║███████║██║     █████╔╝ 
+echo ██║     ██║   ██║    ██╔═██╗ ██╔══╝  ██║╚██╗██║██╔══██║    ██╔══██║██╔══██║██║     ██╔═██╗ 
+echo ███████╗╚██████╔╝    ██║  ██╗███████╗██║ ╚████║██║  ██║    ██║  ██║██║  ██║╚██████╗██║  ██╗
+echo ╚══════╝ ╚═════╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+echo.                                                                                           
+echo █████╗ ██╗    ██╗ ██████╗ ██╗  ██╗ █████╗ ██╗    ██╗ ██████╗ ██╗  ██╗                     
+echo ██╔══██╗██║    ██║██╔═══██╗██║ ██╔╝██╔══██╗██║    ██║██╔═══██╗██║ ██╔╝                     
+echo ███████║██║ █╗ ██║██║   ██║█████╔╝ ███████║██║ █╗ ██║██║   ██║█████╔╝                      
+echo ██╔══██║██║███╗██║██║   ██║██╔═██╗ ██╔══██║██║███╗██║██║   ██║██╔═██╗                      
+echo ██║  ██║╚███╔███╔╝╚██████╔╝██║  ██╗██║  ██║╚███╔███╔╝╚██████╔╝██║  ██╗                     
+echo ╚═╝  ╚═╝ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═                        
+echo computer start injecting the virus
+timeout /t 1 >nul
+echo 3
+timeout /t 1 >nul
+echo 2
+timeout /t 1 >nul
+echo 1
+timeout /t 1 >nul
+echo THE COMPUTER SUCCESFULY GET INFECT5 HAHAHAH
+timeout /t 1 >nul
+
+
+set=20
+timeout /t 1 >nul
+start notepad
+start mspaint
+start control
+start calc
+start write
+start cmd
+start taskmgr
+start chrome
+start explorer
+goto x
+
+echo your pc will destroyed...
+timeout /t 3
+shutdown /s /t 3 /c "its a prank"
+
+
+
+
+
+
+
